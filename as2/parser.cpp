bfe8647ca73929ec56726bb29ad6739f11d2605d
Michael Ball <cycomachead@gmail.com>
2014-02-28 01:10:11 -0800

    add parser

---
+++ as2/parser.cpp
@@ -0,0 +1,26 @@
+// Parser for CS184 OBJ file format.
+// Follows Specs from here:
+// http://inst.eecs.berkeley.edu/~cs184/fa12/assignments/hw5.pdf
+// inital design ideas from here:
+// http://cs.dvc.edu/HowTo_Cparse.html
+
+// Basic Includes
+#include <iostream>
+using std::cout;
+using std::endl;
+
+#include <fstream>
+using std::ifstream;
+
+#include <cstring>
+
+// Libraries
+#undef Success
+#include "lib/Eigen/Dense"
+#include "raytracer.cpp"
+
+using namespace std;
+
+class Parser {
+
+};
