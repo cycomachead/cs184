aaa85250579809b667f7ab998d5c3f565465b9d6
Michael Ball <cycomachead@gmail.com>
2014-03-11 15:53:32 -0700

    add new header

---
+++ as2/raytracer.h
@@ -0,0 +1,23 @@
+// Michael Ball CS184 RayTracer Header
+
+#include <cstdint>
+#include <cmath>
+#include <cstdlib>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+
+// Image File Writing
+#include "lib/lodepng.h"
+
+// X11 and Eigen don't play nicely...
+#undef Success
+#include "lib/Eigen/Eigen"
+
+// Class Includes
+
+
+// Main Functions
+void argParse(int argc, const char** argv);
+
+// Handy Stuff
