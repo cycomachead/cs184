e116fba8ce713f935df3bc46af91db2166a3945a
Michael Ball <cycomachead@gmail.com>
2014-03-14 23:41:05 -0700

    dammmmmmmnnnnnnn

---
+++ as2/camera.cpp
@@ -0,0 +1,62 @@
+//
+//
+
+#include "camera.h"
+
+#include "simple.h"
+#include "sampler.h"
+#include "film.h"
+
+Camera::Camera(Point from, Point at, Point u, float f, int w, int h) {
+    float width, height, aspect;
+    lookFrom = from;
+    lookAt = at;
+    up = u;
+    fovY = f;
+    pixelWidth = w;
+    pixelHeight = h;
+
+
+    aspect =  pixelWidth/pixelHeight;
+    height = 2 * tan(M_PI/180 * .5 * fovY);
+    width = height * aspect;
+
+
+    z = lookAt - lookFrom;
+    x = z.cross(up);
+    y = x.cross(z);
+    x.normalize(); // Is normalization necessary? Some suggest it....
+    y.normalize();
+    z.normalize();
+
+    deltaX = x * width / pixelWidth;
+    deltaY = y * height / pixelHeight;
+
+    centerX = pixelWidth/2.0 + 0.5; // 0.5 == pixel centers
+    centerY = pixelHeight/2.0 + 0.5;
+
+    // ORIGINCAL FORMULA
+    // deltaY * (sample.y - centerY) + z + deltaX * (sample.x - centerX)
+    // CAN I FACTOR???
+    // dirConst = deltaY * -1 * centerY + z + deltaX * -1 * centerX;
+
+}
+
+// modify poiter parameter.
+void Camera::generateRay(Sample& sample, Ray* ray) {
+    ray->pos  = lookFrom;
+    ray->tMin = 0.0f;
+    ray->tMax = FLT_MAX;
+    // The only interesting calculation:
+    // (dir is a vector3f)
+    ray->dir  = deltaY * (sample.y - centerY) + z + deltaX * (sample.x - centerX);
+}
+
+// same function but return a new ray
+Ray Camera::generateRay(Sample& sample) {
+    return Ray(
+        lookFrom,
+        deltaY * (sample.y - centerY) + z + deltaX * (sample.x - centerX),
+        0.0f,
+        FLT_MAX);
+}
---
+++ as2/objects.cpp
@@ -0,0 +1,13 @@
+//
+//
+
+#include "objects.h"
+
+bool Primitive::intersect(Ray& ray, float* thit, Intersection* in) {
+    return false;
+}
+
+//
+bool Primitive::intersect(Ray& ray) {
+    return false;
+}
