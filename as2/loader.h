2118a97230a2024ffb7ff533daf5b04b81a0e5de
Michael Ball <cycomachead@gmail.com>
2014-03-16 22:55:14 -0700

    adding parser

---
+++ as2/loader.h
@@ -0,0 +1,21 @@
+// PARSER HEADER
+//
+
+#ifndef LOADER_H
+#define LOADER_H
+
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <stack>
+#include <iostream>
+
+#include "common.h"
+#include "simple.h"
+#include "scene.h"
+#include "brdf.h"
+#include "objects.h"
+
+void loadX(string file);
+
+#endif
