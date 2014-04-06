1d03cc812797c0913769832f0775a576e2b2e811
Tomo <tueda14@gmail.com>
2014-04-06 14:03:07 -0700

    added header file for bezier mini-library

---
+++ as3/bezier.h
@@ -0,0 +1,17 @@
+#ifndef bezier_h
+#define bezier_h
+
+#include "as3.h"
+
+/** Uniform Tesselation functions. Look at cpp file for each function
+	description.**/
+
+glm::vec4 bezcurveinterp(vector< glm::vec4 > curve, float u, glm::vec4* dPdu);
+vector< vector<glm::vec4> > transpose(vector< vector< glm::vec4 > > patch);
+glm::vec4 normalize(glm::vec4 point);
+glm::vec4 cross(glm::vec4 a, glm::vec4 b);
+glm::vec4 bezpatchinterp(vector< vector< glm::vec4 > > patch, float u, float v, glm::vec4* normal);
+void subdividepatch(vector< vector< glm::vec4 > > patch, float step, 
+	vector< glm::vec4 >* p, vector< glm::vec4 >* n);
+
+#endif
